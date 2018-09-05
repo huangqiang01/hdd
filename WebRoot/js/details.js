396bc34f5cb332f80f283b149d2b4892f720e0c7
huangqiang <547258572@qq.com>
2018-09-05 11:00:28 +0800

    11

--- WebRoot/js/details.js
+++ WebRoot/js/details.js
@@ -15,6 +15,13 @@ $(document).ready(function() {
 			$('.guankan').html(result.checknum);
 			// 点赞人数
 			$('.dianzan').html(result.likenum);
+			var arraycontent = result.arraycontent;
+			if (arraycontent) {
+				// 描述
+				$('.miaoshu11').html(arraycontent);
+			}
+			// 处理分享
+			shareCon(result.arrayname, arraycontent);
 			// 获取图片
 			getImg(minid);
 			if (isCheck(minid)) {
@@ -130,6 +137,81 @@ function isClickLike(id) {
 	}
 	window.localStorage.setItem('_likeimgdetails_', _likeimgdetails_);
 	return true;
+}
+
+// 分享内容
+function shareCon(title, content) {
+	$.ajax({
+		url: "/app/signatureServlet?param=signature",
+		type: "post",
+		dataType: "json",
+		success: function(data){
+			// 页面功能授权
+			wxToFunction(data, title, content);
+		}
+	});
+}
+
+//页面功能授权
+function wxToFunction(data, title, content){
+	wx.config({
+	    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+	    appId: data.appId, // 必填，公众号的唯一标识
+	    timestamp: data.timestamp, // 必填，生成签名的时间戳
+	    nonceStr: data.noncestr, // 必填，生成签名的随机串
+	    signature: data.signature,// 必填，签名，见附录1
+	    jsApiList: ["onMenuShareTimeline", "onMenuShareAppMessage", "onMenuShareQQ"] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+	});
+	
+	var _uri = location.protocol + '//' + location.host;
+	wx.ready(function(){
+		wx.onMenuShareTimeline({
+		    title: title, // 分享标题
+		    link: window.location.href, // 分享链接
+		    imgUrl: _uri + '/app/img/mark.jpg', // 分享图标
+		    success: function () { 
+		        // 用户确认分享后执行的回调函数
+//		    	alert("成功");
+		    },
+		    cancel: function () { 
+		        // 用户取消分享后执行的回调函数
+//		    	alert("取消");
+		    }
+		});
+		wx.onMenuShareAppMessage({
+		    title: title, // 分享标题
+		    desc: content, // 分享描述
+		    link: window.location.href, // 分享链接
+		    imgUrl: _uri + '/app/img/mark.jpg', // 分享图标
+		    type: '', // 分享类型,music、video或link，不填默认为link
+		    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+		    success: function () { 
+		        // 用户确认分享后执行的回调函数
+//		       alert("成功");
+		    },
+		    cancel: function () { 
+		        // 用户取消分享后执行的回调函数
+//		      alert("取消");
+		    }
+		});
+		
+		wx.onMenuShareQQ({
+		    title: title, // 分享标题
+		    desc: content, // 分享描述
+		    link: window.location.href, // 分享链接
+		    imgUrl: _uri + '/app/img/mark.jpg', // 分享图标
+		    success: function () { 
+		       // 用户确认分享后执行的回调函数
+		    },
+		    cancel: function () { 
+		       // 用户取消分享后执行的回调函数
+		    }
+		});
+	});
+	
+	wx.error(function(res){
+		// alert(res);
+	});
 }
 
 
